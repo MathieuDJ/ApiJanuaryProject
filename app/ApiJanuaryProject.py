247be1ba52381c6cbc7b10522ab1f81d03aad6bd
Mathieu Du Jardin <72858870+MathieuDJ@users.noreply.github.com>
2023-01-05 13:37:35 +0100

    Update ApiJanuaryProject.py

--- app/ApiJanuaryProject.py
+++ app/ApiJanuaryProject.py
@@ -13,7 +13,8 @@ origins = [
     "http://127.0.0.1:63342",
     "https://randomizer-service-mathieudj.cloud.okteto.net",
     "https://randomizer-service-mathieudj.cloud.okteto.net/auto/",
-    "https://randomizer-service-mathieudj.cloud.okteto.net/auto"
+    "https://randomizer-service-mathieudj.cloud.okteto.net/auto",
+    "https://sage-cannoli-35a40c.netlify.app/"
 ]
 
 app.add_middleware(
