3262333773c8685a2d7b68a8e5adcb1a7143f590
Mathieu Du Jardin <72858870+MathieuDJ@users.noreply.github.com>
2023-01-06 13:32:25 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -10,17 +10,3 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-
-    cars = relationship("Car", back_populates="owner")
-
-
-class Car(Base):
-    __tablename__ = "cars"
-
-    id = Column(Integer, primary_key=True, index=True)
-    make = Column(String, index=True)
-    model = Column(String, index=True)
-    color = Column(String, index=True)
-    horsepower = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="cars")
