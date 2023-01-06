7c1db92e55110f0af223fac7ad6005ef39e142d8
Mathieu Du Jardin <72858870+MathieuDJ@users.noreply.github.com>
2023-01-06 15:09:56 +0100

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 
 import models
 import schemas
-
+import auth
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -17,8 +17,8 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "ishashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    hashed_password = auth.get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
